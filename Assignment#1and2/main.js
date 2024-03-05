4dac4154a07a55b076db64cfe5da1b8f3987114d
ERUM WARIS <159144847+erum-waris@users.noreply.github.com>
2024-03-05 17:10:11 +0500

    typescript
    Assignment 1 and 2

---
+++ Assignment#1and2/main.js
@@ -0,0 +1,9 @@
+//  Assignment # 01
+// 1.	Install Node.js, TypeScript and VS Code on your computer
+//  Assignment # 2
+var personName = "HEY! Eric,,";
+var message = "HELLO!  Would you like to learn some python today?? ";
+console.log(personName, message);
+var Senior = "SIR ZIA KHAN";
+var message1 = "He is a Leader of the Faculty ";
+console.log("".concat(Senior, " , ").concat(message1));
---
+++ Assignment#1and2/main.ts
@@ -0,0 +1,30 @@
+//  Assignment # 01
+
+// 1.	Install Node.js, TypeScript and VS Code on your computer
+
+//  Assignment # 2
+ 
+ let personName : string = "HEY! Eric,,"
+ let message : string = "HELLO!  Would you like to learn some python today?? "
+ 
+ console.log(personName,message);
+
+let Senior : string = "SIR ZIA KHAN";
+let message1 :string = "He is a Leader of the Faculty ";
+console.log(`${Senior} , ${message1}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
